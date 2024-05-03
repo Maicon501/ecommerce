28051623afb628a88692ca869ebae81cc4fc3925
Maicon501 <maicondmonteiro@gmail.com>
2024-05-02 21:22:47 -0300

    tpl site

---
+++ vendor/hcodebr/php-classes/src/Page.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace hcode;
+
+use Rain\tpl;
+
+class Page {
+    private $tpl;
+    private $options = [];
+    private $defaults = [
+        "data"=>[]
+
+    ];
+    public function __construct($opts = array()){
+    
+         $this->optios = array_merge($this->defaults, $opts);
+
+         $config = array(
+		      "tpl_dir"       => $_SERVER["DOCUMENT_ROOT"]."/views/",
+		      "cache_dir"     => $_SERVER["DOCUMENT_ROOT"]."/views-cache/",
+		      "debug"         => false
+	     );
+
+	     Tpl::configure( $config );
+     
+         $this->tpl = new Tpl;
+         
+         $this->setData($this->options["data"]);
+         $this->tpl->draw("headerer");
+
+    } 
+    
+    private function setData($data = array())
+    {
+
+           foreach ($data as $key => $value) {
+         	$this=>tpl->assingn($key, $value);
+         }       
+
+    }
+
+    public function setTpl($nome, $data = array(), $returnHTML = false)
+    {
+
+         $this->setData($data);
+
+         return $this->tpl->draw($name, $returnHTML);
+    }
+
+
+    public function __destruct(){
+
+              $this->tpl->draw("footer");
+
+    }
+
+}
+?>
